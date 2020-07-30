35cd33ca219af62560c02a58db34bfc3828014a9
Ahmad Abbas <abbasa4696@gmail.com>
2020-07-30 23:34:48 +0300

    vcard system v7

--- routes/digitalcard.js
+++ routes/digitalcard.js
@@ -40,13 +40,17 @@ router.get('/update/:id', (req, res) => {
     });
 });
 
-router.post('/add', upload.single('logo'), (req, res) => {
-    console.log(req.body);
-
+router.post('/add', upload.fields([{
+    name: 'logo', maxCount: 1
+  }, {
+    name: 'background', maxCount: 1
+  }]), (req, res) => {
+    console.log(req.files['logo'][0]);
     fs.readFile('localbase/vcards.json', (err, data) => {
         if (err) throw err;
 
-        const filePath = req.file.path;
+        const filePath1 = req.files['logo'][0].path;
+        const filePath2 = req.files['background'][0].path;
 
         let phoneList = [];
         for(let i=1; i<=req.body.phoneCount; i++){
@@ -61,39 +65,48 @@ router.post('/add', upload.single('logo'), (req, res) => {
 
         vcards = JSON.parse(data);
 
-        fs.rename(filePath, filePath+'.png', () => {
-            vcards.push( 
-                {
-                    business: req.body.business,
-                    logo : req.file.filename+'.png',
-                    bg: {link: req.body.bglink, type: req.body.bgtype },
-                    facebook: {title: req.body.facebook1, link: req.body.facebook2 },
-                    waze: {title: req.body.waze1, link: req.body.waze2 },
-                    instagram: {title: req.body.instagram1, link: req.body.instagram2 },
-                    whatsrouter: {title: req.body.whatsrouter1, link: req.body.whatsrouter2 },
-                    phones : phoneList,
-                    day1 : {start:req.body.days1, end: req.body.daye1},
-                    day2 : {start:req.body.days2, end: req.body.daye2},
-                    day3 : {start:req.body.days3, end: req.body.daye3},
-                    day4 : {start:req.body.days4, end: req.body.daye4},
-                    day5 : {start:req.body.days5, end: req.body.daye5},
-                    day6 : {start:req.body.days6, end: req.body.daye6},
-                    day7 : {start:req.body.days7, end: req.body.daye7},
-                }
-            );
-
-            fs.writeFile('localbase/vcards.json', JSON.stringify(vcards) , function (err) {
-                if (err) throw err;
-                    
-                res.redirect('/digitalcard/admin');
+        fs.rename(filePath1, filePath1+'.png', () => {
+            fs.rename(filePath2, filePath2+'.png', () => {
+                vcards.push( 
+                    {
+                        business: req.body.business,
+                        logo : req.files['logo'][0].filename+'.png',
+                        bg: {link: req.files['background'][0].filename+'.png', type: req.body.bgtype },
+                        colors: {primary: req.body.primary, secondary: req.body.secondary},
+                        facebook: {title: req.body.facebook1, link: req.body.facebook2 },
+                        waze: {title: req.body.waze1, link: req.body.waze2 },
+                        instagram: {title: req.body.instagram1, link: req.body.instagram2 },
+                        whatsrouter: {title: req.body.whatsrouter1, link: req.body.whatsrouter2 },
+                        phones : phoneList,
+                        day1 : {start:req.body.days1, end: req.body.daye1},
+                        day2 : {start:req.body.days2, end: req.body.daye2},
+                        day3 : {start:req.body.days3, end: req.body.daye3},
+                        day4 : {start:req.body.days4, end: req.body.daye4},
+                        day5 : {start:req.body.days5, end: req.body.daye5},
+                        day6 : {start:req.body.days6, end: req.body.daye6},
+                        day7 : {start:req.body.days7, end: req.body.daye7},
+                    }
+                );
+    
+                fs.writeFile('localbase/vcards.json', JSON.stringify(vcards) , function (err) {
+                    if (err) throw err;
+                        
+                    res.redirect('/digitalcard/admin');
+                });
             });
         });
     });
 });
 
-router.post('/update/:id', upload.single('logo'), (req, res) => {
+router.post('/update/:id',  upload.fields([{
+    name: 'logo', maxCount: 1
+  }, {
+    name: 'background', maxCount: 1
+  }]), (req, res) => {
     fs.readFile('localbase/vcards.json', (err, data) => {
-        const filePath = req.file.path;
+
+        const filePath1 = req.files['logo'][0].path;
+        const filePath2 = req.files['background'][0].path;
             
         let phoneList = [];
         for(let i=1; i<= req.body.phoneCount; i++){
@@ -108,33 +121,35 @@ router.post('/update/:id', upload.single('logo'), (req, res) => {
         if (err) throw err;
 
         vcards = JSON.parse(data);
-        fs.rename(filePath, filePath+'.png', () => {
+        fs.rename(filePath1, filePath1+'.png', () => {
+            fs.rename(filePath2, filePath2+'.png', () => {
                 let vcard =  
-                {
-                    business: req.body.business,
-                    logo : req.file.filename+'.png',
-                    bg: {link: req.body.bglink, type: req.body.bgtype },
-                    facebook: {title: req.body.facebook1, link: req.body.facebook2 },
-                    waze: {title: req.body.waze1, link: req.body.waze2},
-                    instagram: {title: req.body.instagram1, link: req.body.instagram2 },
-                    whatsrouter: {title: req.body.whatsrouter1, link: req.body.whatsrouter2 },
-                    phones : phoneList,
-                    day1 : {start:req.body.days1, end: req.body.daye1},
-                    day2 : {start:req.body.days2, end: req.body.daye2},
-                    day3 : {start:req.body.days3, end: req.body.daye3},
-                    day4 : {start:req.body.days4, end: req.body.daye4},
-                    day5 : {start:req.body.days5, end: req.body.daye5},
-                    day6 : {start:req.body.days6, end: req.body.daye6},
-                    day7 : {start:req.body.days7, end: req.body.daye7},
-                };
+                    {
+                        business: req.body.business,
+                        logo : req.files['logo'][0].filename+'.png',
+                        bg: {link: req.files['background'][0].filename+'.png', type: req.body.bgtype },
+                        colors: {primary: req.body.primary, secondary: req.body.secondary},
+                        facebook: {title: req.body.facebook1, link: req.body.facebook2 },
+                        waze: {title: req.body.waze1, link: req.body.waze2 },
+                        instagram: {title: req.body.instagram1, link: req.body.instagram2 },
+                        whatsrouter: {title: req.body.whatsrouter1, link: req.body.whatsrouter2 },
+                        phones : phoneList,
+                        day1 : {start:req.body.days1, end: req.body.daye1},
+                        day2 : {start:req.body.days2, end: req.body.daye2},
+                        day3 : {start:req.body.days3, end: req.body.daye3},
+                        day4 : {start:req.body.days4, end: req.body.daye4},
+                        day5 : {start:req.body.days5, end: req.body.daye5},
+                        day6 : {start:req.body.days6, end: req.body.daye6},
+                        day7 : {start:req.body.days7, end: req.body.daye7},
+                    };
                 vcards[req.params.id] = vcard;
-
                 fs.writeFile('localbase/vcards.json', JSON.stringify(vcards) , function (err) {
                     if (err) throw err;
                         
                     res.redirect('/digitalcard/admin');
                 });
             });
+        });
     });
 });
 
