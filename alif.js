ec957f2343684187791a215f7e5aa7bcdb175e75
ahmad abbas <abbasa4696@gmail.com>
2020-10-24 09:05:19 +0000

    back up from server

--- alif.js
+++ alif.js
@@ -22,6 +22,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/barcode/saltburger' ,(req, res) => {
+res.redirect('http://www.saltburger.alifalif.work');
+});
+
 app.get('/get/all/:data', (req, res) => {
     fs.readFile('localbase/'+req.params.data+'.json', (err, data) => {
         if (err) throw err;
