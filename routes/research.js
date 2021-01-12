846fadeb0ab94901a6cc476bd5c937a5eae97fd5
Ahmad Abbas <abbasa4696@gmail.com>
2021-01-12 12:40:55 +0200

    edit research

--- routes/research.js
+++ routes/research.js
@@ -5,23 +5,57 @@ const router = express.Router();
 
 // DATA
 router.get('/', (req, res) => {
-    fs.readFile('localbase/research.json', (err, data) => {
-        if (err) throw err;
+    let today = new Date().toISOString().slice(0, 10);
 
-        const research = JSON.parse(data);
+    fs.readFile(`localbase/research${today}.json`, (err, data) => {
+        let research;
+
+        if (err) research = [];
+        else research = JSON.parse(data);
+        
+        let lightSleep = 0,
+        deepSleep = 0,
+        noiseAvg = 0,
+        lightAvg = 0,
+        totalSleep = 0;
+
+        research.forEach(res => {
+            lightSleep += res.lightSleep;
+            deepSleep += res.deepSleep;
+
+            const noise = Number.parseFloat(res.noise);
+            const light = Number.parseFloat(res.light);
+
+            if(noise > 0 && noise < 1000)
+                noiseAvg += noise;
+            if(light > 0 && light < 1000)
+                lightAvg += light;
+        });
+
+        console.log(lightSleep);
+
+        noiseAvg /= research.length;
+        lightAvg /= research.length;
+
+        noiseAvg = noiseAvg.toFixed(2);
+        lightAvg = lightAvg.toFixed(2);
+
+        totalSleep = lightSleep + deepSleep;
         
         fs.readFile('localbase/research_users.json', (err, data) => {
             if (err) throw err;
     
             const users = JSON.parse(data);
-            
-            res.render('research/index', {research, users, userID: 0});
+
+            res.render('research/index', {research, users, userID: 0, lightSleep, deepSleep, noiseAvg, lightAvg, totalSleep});
         });
     }); 
 });
 
 router.post('/', (req, res) => {
-    fs.readFile('localbase/research.json', (err, data) => {
+    let today = new Date().toISOString().slice(0, 10);
+
+    fs.readFile(`localbase/research${today}.json`, (err, data) => {
         if (err) throw err;
 
         const research = JSON.parse(data);
@@ -86,24 +120,54 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    fs.readFile('localbase/research.json', (err, data) => {
-        if (err) throw err;
+    let today = new Date().toISOString().slice(0, 10);
 
-        const allResearch = JSON.parse(data);
+    fs.readFile(`localbase/research${today}.json`, (err, data) => {
+        let allResearch;
+
+        if (err) allResearch = [];
+        else allResearch = JSON.parse(data);
+        
+        let lightSleep = 0,
+        deepSleep = 0,
+        noiseAvg = 0,
+        lightAvg = 0,
+        totalSleep = 0;
+
         let research = [];
 
-        allResearch.forEach(ree => {
-            if(ree.user == req.params.id) {
-                research.push(ree);
+        allResearch.forEach(res => {
+            if(res.user == req.params.id) {
+                research.push(res);
+                
+                lightSleep += res.lightSleep;
+                deepSleep += res.deepSleep;
+    
+                const noise = Number.parseFloat(res.noise);
+                const light = Number.parseFloat(res.light);
+
+                if(noise > 0 && noise < 1000)
+                    noiseAvg += noise;
+                if(light > 0 && light < 1000)
+                    lightAvg += light;
             }
         });
+
+        noiseAvg /= research.length;
+        lightAvg /= research.length;
+
+        noiseAvg = noiseAvg.toFixed(2);
+        lightAvg = lightAvg.toFixed(2);
+
+        totalSleep = lightSleep + deepSleep;
+
 
         fs.readFile('localbase/research_users.json', (err, data) => {
             if (err) throw err;
     
             const users = JSON.parse(data);
             
-            res.render('research/index', {research, users, userID: req.params.id});
+            res.render('research/index', {research, users, userID: req.params.id, lightSleep, deepSleep, noiseAvg, lightAvg, totalSleep});
         });
     });
 });
